73c63f79b850b14246351952a94919d82d5efdd2
Kaveh Korki <kavehkorki@gmail.com>
2023-06-21 21:46:18 +1000

    update directory

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,9 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
-const router = require("./routers/index.router");
+const router = require("../routers/index.router");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -15,6 +14,4 @@ app.use(cors());
 
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+module.exports = app;
