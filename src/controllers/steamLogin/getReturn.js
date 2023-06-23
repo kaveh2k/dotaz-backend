35f54bf58a4edc6604383b94d50112fd56f843ac
Kaveh Korki <kavehkorki@gmail.com>
2023-06-23 17:18:47 +1000

    steam login

--- src/controllers/steamLogin/getReturn.js
+++ src/controllers/steamLogin/getReturn.js
@@ -2,24 +2,26 @@ const jwt = require("jsonwebtoken");
 
 const getReturn = async (req, res, next) => {
   // Generate JWT token
-  const token = jwt.sign(
-    {
-      user: {
-        id: req.user._id,
-        displayName: req.user.displayName,
-        email: req.user.email,
-        avatar: req.user.avatar,
-        profileUrl: req.user.profileUrl,
-        realName: req.user.realName,
-        countryCode: req.user.countryCode,
-      },
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
+  // console.log(req);
+  // const token = jwt.sign(
+  //   {
+  //     user: {
+  //       id: req.user._id,
+  //       displayName: req.user.displayName,
+  //       email: req.user.email,
+  //       avatar: req.user.avatar,
+  //       profileUrl: req.user.profileUrl,
+  //       realName: req.user.realName,
+  //       countryCode: req.user.countryCode,
+  //     },
+  //   },
+  //   process.env.JWT_SECRET,
+  //   { expiresIn: "1h" }
+  // );
 
-  // Redirect to the frontend with the JWT token as a query parameter
-  res.redirect(`${process.env.REDIRECT_URL}?token=${token}`);
+  // // Redirect to the frontend with the JWT token as a query parameter
+  // res.redirect(`${process.env.REDIRECT_URL}?token=${token}`);
+  res.status(200).json({ message: "we got your data" });
 };
 
 module.exports = {
