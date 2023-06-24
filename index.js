131210138cda3af95b19610e75430ab72584a2d8
Kaveh Korki <kavehkorki@gmail.com>
2023-06-24 15:27:02 +1000

    bug fix

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-// session
-const generateRandomSecret = require("./src/services/RandomSecret.service");
-const session = require("express-session");
-const sessionSecret = generateRandomSecret();
 const app = require("./src/config/app");
 
 const PORT = process.env.PORT || 5000;
-
-const { db } = require("./src/config/database");
-const passport = require("./src/config/passport");
-
-//collection to store sessions
-const MongoDBStore = require("connect-mongodb-session")(session);
-
-const store = new MongoDBStore({
-  uri: process.env.MONGODV_URI,
-  collection: "sessions",
-});
-store.on("error", (error) => {
-  console.error("MongoDB session store error:", error);
-});
-app.use(
-  session({
-    secret: sessionSecret,
-    resave: true,
-    saveUninitialized: true,
-    store: store,
-  })
-);
-
-// Database connection
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.on("connected", () => {
-  console.log("Connected to Database");
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Server connection
 app.listen(PORT, () => {
--- src/config/app.js
+++ src/config/app.js
@@ -5,8 +5,25 @@ const express = require("express");
 const helmet = require("helmet");
 const router = require("../routers/index.router");
 
+const { db } = require("./database");
+const passport = require("./passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const generateRandomSecret = require("../services/RandomSecret.service");
 // Set up Express app
 const app = express();
+
+app.use(
+  session({
+    secret: generateRandomSecret(),
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODV_URL,
+    }),
+  })
+);
 
 // Set up Express middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,6 +31,14 @@ app.use(require("cookie-parser")());
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+// Database connection
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("connected", () => {
+  console.log("Connected to Database");
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Middleware to Routes
 app.use("/", router);
