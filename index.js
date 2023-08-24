9ce059723086f83074f887c679fde791d8c1b339
kaveh <hidemek@duck.com>
2023-08-24 05:06:55 +0330

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000;
 
 // Server connection
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT} and witing for database ....`);
+  console.log(`Server is running on ${PORT} and waiting for database .... (no database right now)`);
 });
