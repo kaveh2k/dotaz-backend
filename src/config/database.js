d6848fdd03e3adde68eb9481254fd73a478493c3
kaveh <hidemek@duck.com>
2023-08-24 05:08:22 +0330

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
-const { mongoose } = require("mongoose");
-const User = require("../schema/user.schema");
+// const { mongoose } = require("mongoose");
+//const User = require("../schema/user.schema");
 
 // Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGODV_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+//mongoose.connect(process.env.MONGODV_URL, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true,
+//});
+// const db = mongoose.connection;
 
-module.exports = { db, User };
+// module.exports = { db, User };
