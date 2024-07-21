724a3157925d8e14684e7cb96294e51420ba72d7
Kaveh Korki <kavehkorki@gmail.com>
2024-07-22 04:25:25 +1000

    update CORS

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://dotaz.netlify.app/"];
+const allowedOrigins = [
+  "https://dotaz.netlify.app/",
+  "https://netlify.app/",
+  "https://www.netlify.app/",
+];
 
 module.exports = allowedOrigins;
